5b7d534c560092405598a06413d280c9c476b1d7
Juan Francisco Drewanz <juan@italic.com>
2023-01-09 17:17:15 -0300

    Update queries.ts
    adding product_by_id an improving products_by_cat

--- client/storefront/src/queries/queries.ts
+++ client/storefront/src/queries/queries.ts
@@ -17,9 +17,34 @@ export const GET_PRODUCTS = gql`
 export const GET_PRODUCTS_BY_CAT = gql`
   query Query($catId: Int) {
     getProductsByCat(catId: $catId) {
+      id
+      category
       title
       price
+      avg_rating
+      reviews_count
       imageurl
+    }
+  }
+`;
+
+export const GET_PRODUCT_BY_ID = gql`
+  query Query($prodId: Int) {
+    getProductById(prodId: $prodId) {
+      avg_rating
+      category
+      id
+      price
+      imageurl
+      reviews_count
+      title
+      specifications {
+        color
+        dimensions
+        material
+        origin
+        weight
+      }
     }
   }
 `;
