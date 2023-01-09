1bdb244604c62d283ede40ffdf35d1dc2b664502
Juan Francisco Drewanz <juan@italic.com>
2023-01-09 17:20:25 -0300

    Update index.tsx
    new landing page

--- client/storefront/pages/index.tsx
+++ client/storefront/pages/index.tsx
@@ -15,9 +15,48 @@ export default function Home() {
       </Head>
       <main className='content-center justify-center bg-teal-100'>
         <div className='content-center justify-center'>
-          <h1 className='font-bold text-7xl text-teal-500'>eMall</h1>
-          <h3>Enjoy your shopping time</h3>
-          <Link href={'/categories'}>Explore</Link>
+          <section>
+            <div className='bg-black text-white py-20 h-full'>
+              <div className='container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24'>
+                <div className='flex flex-col w-full lg:w-1/3 justify-center items-start p-8'>
+                  <h1 className='text-3xl md:text-5xl p-2 text-teal-500 tracking-loose'>
+                    eMall
+                  </h1>
+                  <h2 className='text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2'>
+                    Enjoy your shopping time
+                  </h2>
+                  <p className='text-sm md:text-base text-gray-50 mb-4'>
+                    Explore our catalog and discover great deals. Register now
+                    to enhance your experience
+                  </p>
+                  <div className='flex flex-row'>
+                    <a
+                      href='/categories'
+                      className='bg-transparent hover:bg-teal-300 text-teal-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-teal-300 hover:border-transparent'
+                    >
+                      Explore
+                    </a>
+                    <a
+                      href='/login'
+                      className='ml-8 w-20 bg-transparent hover:bg-white text-white-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-white-300 hover:border-transparent'
+                    >
+                      Login
+                    </a>
+                  </div>
+                </div>
+                <div className='p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center'>
+                  <div className='h-48 flex flex-wrap content-center'>
+                    <div>
+                      <img
+                        className='inline-block mt-24 md:mt-0 p-8 md:p-0 rounded-lg'
+                        src='https://images.pexels.com/photos/3769747/pexels-photo-3769747.jpeg'
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </main>
     </>
