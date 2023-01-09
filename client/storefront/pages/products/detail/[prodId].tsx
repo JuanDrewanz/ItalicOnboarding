2e592026eacc61f244e11987d1dec07270015638
Juan Francisco Drewanz <juan@italic.com>
2023-01-09 17:16:25 -0300

    Create [prodId].tsx
    dynamic routes for a product's detail

---
+++ client/storefront/pages/products/detail/[prodId].tsx
@@ -0,0 +1,61 @@
+import { useQuery } from '@apollo/client';
+import { useRouter } from 'next/router';
+import client from '../../../apolo-client';
+import NavBar from '../../../src/components/NavBar';
+import ProductDetail from '../../../src/components/ProductDetail';
+import { GET_PRODUCT_BY_ID } from '../../../src/queries/queries';
+
+export async function getServerSideProps() {
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
+
+function GetDetails({ prodId }) {
+  const { loading, error, data } = useQuery(GET_PRODUCT_BY_ID, {
+    client: client,
+    variables: { prodId },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  const { title, avg_rating, category, id, price, imageurl, reviews_count } =
+    data.getProductById;
+
+  const { color, dimensions, material, origin, weight } =
+    data.getProductById.specifications;
+  return (
+    <div>
+      <h1>Holis</h1>
+      <ProductDetail
+        title={title}
+        avg_rating={avg_rating}
+        category={category}
+        id={id}
+        price={price}
+        imageurl={imageurl}
+        reviews_count={reviews_count}
+        color={color}
+        dimensions={dimensions}
+        material={material}
+        origin={origin}
+        weight={weight}
+      />
+    </div>
+  );
+}
+
+export default function DetailHome() {
+  const router = useRouter();
+  const { prodId } = router.query;
+  const prodId_int: number = parseInt(prodId);
+
+  return (
+    <div className='justify-center flex flex-col w-screen'>
+      <NavBar />
+      <h1>Product Detail</h1>
+      <GetDetails prodId={prodId_int} />
+    </div>
+  );
+}
