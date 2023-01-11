4e6aeb2c1dccd8fb41df182736a0fbb32a8d2345
Juan Francisco Drewanz <juan@italic.com>
2023-01-11 17:31:04 -0300

    user login and register FE

--- client/storefront/pages/login.tsx
+++ client/storefront/pages/login.tsx
@@ -1,4 +1,85 @@
+import { useMutation } from '@apollo/client';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import client from '../apolo-client';
+import { LOGIN_USER } from '../src/queries/queries';
+
+function LoginUser() {
+  const router = useRouter();
+
+  const [loginUser, { error, data, loading }] = useMutation(LOGIN_USER, {
+    client: client,
+    onCompleted: ({ data }) => {
+      localStorage.setItem('token', 'prueba');
+      router.push('/categories');
+    },
+  });
+
+  const [newUser, setNewUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await loginUser({
+        variables: {
+          user: newUser,
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return (
+    <form>
+      <p className='mb-4'>Please login to your account</p>
+      <div className='mb-4'>
+        <input
+          type='text'
+          name='email'
+          className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+          id='exampleFormControlInput1'
+          placeholder='E-mail'
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <div className='mb-4'>
+        <input
+          type='password'
+          name='password'
+          className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+          id='exampleFormControlInput1'
+          placeholder='Password'
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <div className='text-center pt-1 mb-12 pb-1'>
+        <button
+          onClick={(e) => handleClick(e)}
+          className='bg-teal-700 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-teal-500 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3'
+          type='button'
+          data-mdb-ripple='true'
+          data-mdb-ripple-color='light'
+        >
+          Log in
+        </button>
+      </div>
+    </form>
+  );
+}
 
 export default function login() {
   const router = useRouter();
@@ -26,50 +107,19 @@ export default function login() {
                         Welcome to eMall
                       </h4>
                     </div>
-                    <form>
-                      <p className='mb-4'>Please login to your account</p>
-                      <div className='mb-4'>
-                        <input
-                          type='text'
-                          className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
-                          id='exampleFormControlInput1'
-                          placeholder='Username'
-                        />
-                      </div>
-                      <div className='mb-4'>
-                        <input
-                          type='password'
-                          className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
-                          id='exampleFormControlInput1'
-                          placeholder='Password'
-                        />
-                      </div>
-                      <div className='text-center pt-1 mb-12 pb-1'>
-                        <button
-                          className='bg-teal-700 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-teal-500 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3'
-                          type='button'
-                          data-mdb-ripple='true'
-                          data-mdb-ripple-color='light'
-                        >
-                          Log in
-                        </button>
-                        <a className='text-gray-500' href='#!'>
-                          Forgot password?
-                        </a>
-                      </div>
-                      <div className='flex items-center justify-between pb-6'>
-                        <p className='mb-0 mr-2'>Don't have an account?</p>
-                        <button
-                          onClick={redirect}
-                          type='button'
-                          className='inline-block px-6 py-2 border-2 border-teal-800 text-teal-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out'
-                          data-mdb-ripple='true'
-                          data-mdb-ripple-color='light'
-                        >
-                          Sign up
-                        </button>
-                      </div>
-                    </form>
+                    <LoginUser />
+                    <div className='flex items-center justify-between pb-6'>
+                      <p className='mb-0 mr-2'>Don't have an account?</p>
+                      <button
+                        onClick={redirect}
+                        type='button'
+                        className='inline-block px-6 py-2 border-2 border-teal-800 text-teal-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out'
+                        data-mdb-ripple='true'
+                        data-mdb-ripple-color='light'
+                      >
+                        Sign up
+                      </button>
+                    </div>
                   </div>
                 </div>
                 <div className='bg-teal-800 lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none'>
--- client/storefront/pages/register.tsx
+++ client/storefront/pages/register.tsx
@@ -1,4 +1,109 @@
+import { gql, useMutation } from '@apollo/client';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { consumers } from 'stream';
+import client from '../apolo-client';
+import { REGISTER_USER } from '../src/queries/queries';
+
+function RegisterUser() {
+  const router = useRouter();
+  const [registerUser, { loading, error, data }] = useMutation(REGISTER_USER, {
+    client: client,
+    onCompleted: () => {
+      router.push('/categories');
+    },
+  });
+
+  const [newUser, setNewUser] = useState({
+    email: '',
+    password: '',
+    username: '',
+  });
+
+  const handleChange = (e) => {
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await registerUser({
+        variables: {
+          user: newUser,
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return (
+    <div>
+      <form>
+        <p className='mb-4'>Please register to enjoy unlimited benefits</p>
+        <div className='mb-4'>
+          <input
+            value={newUser.email}
+            name='email'
+            type='text'
+            className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+            id='exampleFormControlInput1'
+            placeholder='E-mail'
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className='mb-4'>
+          <input
+            value={newUser.username}
+            name='username'
+            type='text'
+            className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+            id='exampleFormControlInput1'
+            placeholder='Username'
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className='mb-4'>
+          <input
+            value={newUser.password}
+            name='password'
+            type='password'
+            className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+            id='exampleFormControlInput1'
+            placeholder='Password'
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className='text-center pt-1 mb-12 pb-1'>
+          <button
+            onClick={(e) => handleClick(e)}
+            className='bg-teal-700 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-teal-500 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3'
+            type='button'
+            data-mdb-ripple='true'
+            data-mdb-ripple-color='light'
+          >
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 export default function register() {
+  const router = useRouter();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.push('/login');
+  };
+
   return (
     <section className='h-full gradient-form bg-gray-900 md:h-screen'>
       <div className='container py-12 px-6 h-full'>
@@ -18,59 +123,19 @@ export default function register() {
                         Welcome to eMall
                       </h4>
                     </div>
-                    <form>
-                      <p className='mb-4'>
-                        Please register to enjoy unlimited benefits
-                      </p>
-                      <div className='mb-4'>
-                        <input
-                          type='text'
-                          className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
-                          id='exampleFormControlInput1'
-                          placeholder='E-mail'
-                        />
-                      </div>
-                      <div className='mb-4'>
-                        <input
-                          type='text'
-                          className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
-                          id='exampleFormControlInput1'
-                          placeholder='Username'
-                        />
-                      </div>
-                      <div className='mb-4'>
-                        <input
-                          type='password'
-                          className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
-                          id='exampleFormControlInput1'
-                          placeholder='Password'
-                        />
-                      </div>
-                      <div className='text-center pt-1 mb-12 pb-1'>
-                        <button
-                          className='bg-teal-700 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-teal-500 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3'
-                          type='button'
-                          data-mdb-ripple='true'
-                          data-mdb-ripple-color='light'
-                        >
-                          Log in
-                        </button>
-                        <a className='text-gray-500' href='#!'>
-                          Forgot password?
-                        </a>
-                      </div>
-                      <div className='flex items-center justify-between pb-6'>
-                        <p className='mb-0 mr-2'>Don't have an account?</p>
-                        <button
-                          type='button'
-                          className='inline-block px-6 py-2 border-2 border-teal-800 text-teal-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out'
-                          data-mdb-ripple='true'
-                          data-mdb-ripple-color='light'
-                        >
-                          Sign up
-                        </button>
-                      </div>
-                    </form>
+                    <RegisterUser />
+                    <div className='flex items-center justify-between pb-6'>
+                      <p className='mb-0 mr-2'>Already have an account?</p>
+                      <button
+                        onClick={(e) => handleClick(e)}
+                        type='button'
+                        className='inline-block px-6 py-2 border-2 border-teal-800 text-teal-800 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out'
+                        data-mdb-ripple='true'
+                        data-mdb-ripple-color='light'
+                      >
+                        Sign in
+                      </button>
+                    </div>
                   </div>
                 </div>
                 <div className='bg-teal-800 lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none'>
